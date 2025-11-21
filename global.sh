022b3384cff919b6321b1244bd050b20a4973c6b
cuadratico <69270131+cuadratico@users.noreply.github.com>
2025-11-21 12:27:05 +0100

    Add files via upload

--- global.sh
+++ global.sh
@@ -6,30 +6,35 @@ printf "\n"
 }
 
 echo -e "\e[38;2;184;255;68m
-  _   _  ___           _ _        _    _   _ 
+  _   _  ___           _ _        _    _   _
  | \ | |/ _ \__      _(_) |      / \  | \ | |
  |  \| | | | \ \ /\ / / | |     / _ \ |  \| |
  | |\  | |_| |\ V  V /| | |___ / ___ \| |\  |
  |_| \_|\___/  \_/\_/ |_|_____/_/   \_\_| \_|
 
 \e[0m"
-
 line
 
 while true; do
-if [ $(ifconfig | grep -oE "wlan0") == "" ]; then
-echo -e "The Wlan0 network interface is not operational\n"
+if [ -z $(ifconfig | grep -oE "wlan0") ]; then
+	echo -e "The Wlan0 network interface is not operational\n"
+	line
 else
-echo -e "Operational Wlan0 network interface\n"
-break
+	echo -e "Operational Wlan0 network interface\n"
+	line
+	break
 fi
 
 sleep 2
 done
 
-# echo -e "IP-local: $(ifconfig | grep -oE "inet ([0-9]{1,3}\.){3}[0-9]{1,3}" | awk '{print $2}')\n"
+show() {
+echo -e "IP-local: $(ifconfig | grep -oE "inet ([0-9]{1,3}\.){3}[0-9]{1,3}" | awk '{print $2}' | grep -vE "^127")\n"
+}
 
+show
 while true; do
+
 
 	read -p "What do you want to do with TCP? (-h): " wh
 	line
@@ -37,6 +42,7 @@ while true; do
 	case $wh in
 
 		cre)
+			show
 			read -p "Specify the port: " port
 			line
 			echo -e "Waiting for a device to connect...\n"
@@ -46,6 +52,7 @@ while true; do
 		;;
 
 		con)
+			show
 			read -p "Specify the port: " port
 			line
 			read -s -p "Specify the IP: " ip
@@ -70,7 +77,8 @@ while true; do
 		;;
 
 		-v)
-			echo -e "--NowiLAN-- V 0.0.1\n"
+			echo -e "--NowiLAN-- V 0.0.2\n"
+			echo -e "In this version, the IP regex function and the wlan0 interface condition have been corrected.\n"
 		;;
 		-h)
 			echo "With the command (cre) you can create a TCP server"
@@ -79,6 +87,11 @@ while true; do
 			echo "With the command (-v) you can see which version of NowiLAN you are using"
 			echo -e "You can exit NowiLAN with the command (exit)\n"
 
+		;;
+
+		*)
+			echo "The command ($wh) does not exist"
+			echo -e "Use the (-h) command to see the rest of the commands\n"
 		;;
 
  	esac
